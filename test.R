3b16d3112a17d17b6fbf5024e52a92d96e1b7e63
David J. Harris <dave@harris-research.net>
2018-01-17 20:36:10 -0500

    Train-test split, more graphs, etc.

--- test.R
+++ test.R
@@ -1,7 +1,10 @@
+set.seed(1)
 source("01-import.R")
 library(brms, quietly = TRUE)
 theme_set(theme_default())
 
+patient_ids = readLines("PRO-ACT/Archive/ALS_Prize_slopes/slopes.train") %>% 
+  gsub("^.*:(.*)\t.*", "\\1", .)
 
 history = history %>% 
   distinct(subject_id, Onset_Delta)
@@ -9,10 +12,10 @@ history = history %>%
 frs = frs %>% 
   select(1:13) %>%
   select(-starts_with("Q5")) %>% 
+  filter(subject_id %in% patient_ids) %>% 
   inner_join(distinct(history), by = "subject_id") %>% 
   na.omit() %>% 
-  mutate(elapsed = (ALSFRS_Delta - Onset_Delta) / 365.24) %>% 
-  slice(1:1000)
+  mutate(elapsed = (ALSFRS_Delta - Onset_Delta) / 365.24)
 
 
 colnames(frs) = gsub("Q..?_", "", colnames(frs))
@@ -25,18 +28,29 @@ responses = c("Speech", "Salivation", "Swallowing", "Handwriting", "Dressing",
 severity = floor(5 - frs[ , colnames(frs) %in% responses])
 
 
-data = cbind(severity, select(frs, subject, elapsed))
+data = cbind(severity, select(frs, subject, elapsed)) %>% 
+  group_by(subject) %>% 
+  mutate(n_months = n()) %>% 
+  filter(n_months == 12) %>% 
+  arrange(subject, elapsed) %>% 
+  mutate(month = 1:n(), month_3_elapsed = elapsed[3])
+
+subjects = unique(data$subject)
+training_subjects = sample(subjects, floor(length(subjects) / 2))
+
+training_data = data %>% 
+  filter(subject %in% training_subjects | month <= 3)
+
+validation_data = data %>% 
+  anti_join(training_data, by = c("subject", "month"))
 
 fit = brm(
   bf(
-    cbind("Speech", "Salivation", "Swallowing", "Handwriting", "Dressing", 
-          "Turning", "Walking", "Climbing", "Respiratory") ~ 
-      elapsed + 
-      (elapsed | s | subject)
+    cbind("Swallowing", "Respiratory", "Walking") ~ elapsed + (elapsed | s | subject)
   ) + 
     set_rescor(FALSE), 
   family = cumulative(),
-  data = data,
+  data = training_data,
   chains = 2,
   cores = 2,
   refresh = 50,
@@ -49,18 +63,22 @@ saveRDS(fit, file = "fit.rds")
 
 fit
 
-cors = colMeans(as.matrix(fit$fit, "cor_1"))
-linpred = posterior_linpred(fit, transform = TRUE)
-p = apply(linpred[,,1:5], 3, colMeans)
-plot(data$elapsed, p[,1])
-mean(log(p[cbind(1:nrow(p),data[["Swallowing"]])]))
-log(.2)
+gap = 1/20 # units: years
 
-
-times = seq(0, max(data$elapsed), 1/365)
-
-id = sample(data$subject, 1)
+id = sample(validation_data$subject, 1)
 #id = "b"
+
+start_time = if (is.numeric(id)) {
+  validation_data %>% 
+    filter(subject == id) %>% 
+    pull(elapsed) %>% 
+    min()
+} else {
+  start_time = 0
+}
+
+times = seq(start_time, max(data$elapsed), gap)
+
 linpred_baseline = posterior_linpred(
   fit, transform = TRUE, 
   newdata = data.frame(elapsed = times, subject = id),
@@ -72,13 +90,20 @@ symptom = 3
 
 
 linpred_symptom = linpred_baseline[,,grep(symptom_names[symptom], dimnames(linpred_baseline)[[3]])]
-
-subject_data = filter(data, subject == id)
+linpred_symptom = linpred_symptom[times >= start_time, , ]
 
 color_order = c(5, 7, 3, 6, 8)
 
+expected_score = sapply(
+  1:5,
+  function(i){
+    colMeans(linpred_symptom[,,i] * i)
+  }
+) %>% 
+  rowSums()
+
 reshape2::melt(apply(linpred_symptom, 3, colMeans)) %>% 
-  ggplot(aes(x = Var1 / 365, y = value, color = Var2, fill = Var2)) +
+  ggplot(aes(x = (Var1 - 1) * gap, y = value, color = Var2, fill = Var2)) +
   geom_area(position = "identity", alpha = 0.1) + 
   geom_line(size = 1, alpha = 0.5)  + 
   colorblindr::scale_color_OkabeIto(order = color_order, use_black = TRUE) + 
@@ -86,5 +111,26 @@ reshape2::melt(apply(linpred_symptom, 3, colMeans)) %>%
   cowplot::theme_cowplot() +
   xlab("Years") +
   ylab("Probability") +
+  xlim(c(0, max(validation_data$elapsed) - min(validation_data$elapsed))) + 
   ylim(c(0, 1.01)) + 
   coord_cartesian(expand = FALSE)
+
+
+
+
+validation_predictions = posterior_predict(fit, newdata = validation_data)
+validation_means = apply(validation_predictions, 3, colMeans)
+
+hist(abs(validation_means[,2] - validation_data$Respiratory), breaks = "fd")
+
+ggplot(
+  data = NULL, 
+  aes(
+    x = validation_data$elapsed - validation_data$month_3_elapsed, 
+    y = abs(validation_means[,2] - validation_data$Respiratory)
+  )
+) +
+  geom_smooth() +
+  cowplot::theme_cowplot()
+
+
